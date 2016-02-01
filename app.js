ba0ee5b11c18618b420fc00565b466faf0db64ca
Jordy Williams <jordythepoet@gmail.com>
2016-01-31 22:02:12 -0600

    after first session, basic angular functionality written

---
+++ app.js
@@ -0,0 +1,26 @@
+angular.module('flapperNews', [])
+
+.controller('MainCtrl', [
+    '$scope',
+    function($scope) {
+        $scope.posts = [
+            {title: 'post 1', upvotes: 5, link: "www.google.com"},
+            {title: 'post 2', upvotes: 2, link: "www.google.com"},
+            {title: 'post 3', upvotes: 15, link: "www.google.com"},
+            {title: 'post 4', upvotes: 9},
+            {title: 'post 5', upvotes: 4}
+        ];
+        $scope.addPost = function(){
+            if(!$scope.title || $scope.title === '') {return;}
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0
+            });
+            $scope.title = "";
+            $scope.link = "";
+        }
+        $scope.incrementUpvotes = function(post){
+            post.upvotes++;
+        }
+    }]);
