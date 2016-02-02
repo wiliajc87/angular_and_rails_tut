b7a9d9cf855dfeeb769e38f26e86aab051864b67
Jordy Williams <jwilliams@leapfrogonline.com>
2016-02-02 16:12:27 -0600

    working on routing/adding comments to posts

--- app.js
+++ app.js
@@ -1,21 +1,52 @@
-angular.module('flapperNews', [])
+angular.module('flapperNews', ['ui.router'])
+.config([
+    '$stateProvider',
+    '$urlRouterProvider',
+    function($stateProvider, $urlRouterProvider){
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: '/home.html',
+                controller: 'MainCtrl'
+            });
+                .state('posts', {
+                url: '/posts/{id}',
+                templateUrl: '/posts.html',
+                controller: 'PostsCtrl'
+            });
+        $urlRouterProvider.otherwise('home');
+    }])
+
+.factory('posts', [function(){
+    var o = {
+        posts: []
+    };
+    return o;
+}])
+
+.controller('PostsCtrl', [
+    '$scope',
+    '$stateParams',
+    'posts',
+    function($scope, $stateParams, posts){
+
+    }]);
 
 .controller('MainCtrl', [
     '$scope',
-    function($scope) {
-        $scope.posts = [
-            {title: 'post 1', upvotes: 5, link: "www.google.com"},
-            {title: 'post 2', upvotes: 2, link: "www.google.com"},
-            {title: 'post 3', upvotes: 15, link: "www.google.com"},
-            {title: 'post 4', upvotes: 9},
-            {title: 'post 5', upvotes: 4}
-        ];
+    'posts',
+    function($scope, posts) {
+        $scope.posts = posts.posts;
         $scope.addPost = function(){
             if(!$scope.title || $scope.title === '') {return;}
             $scope.posts.push({
                 title: $scope.title,
                 link: $scope.link,
-                upvotes: 0
+                upvotes: 0,
+                comments: [
+                    {author: 'Joe', body: 'Cool post!', upvotes: 0},
+                    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+                ]
             });
             $scope.title = "";
             $scope.link = "";
